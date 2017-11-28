76d36299b6f1f7755d156f30c32afc0ad2a120d3
momoxie <wangningxyl@163.com>
2017-11-28 16:06:49 +0800

    修改python执行cmd的方式为os.system

--- src/decompile_apk.py
+++ src/decompile_apk.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 #coding=utf-8
 
-import sys
-import commands
+import os,sys
 
 def open_classyshark(apkfile):
     if apkfile != "":
         print "java -jar ../plugins/classyshark/ClassyShark.jar -open %s"%apkfile
-        commands.getstatusoutput("java -jar ../plugins/classyshark/ClassyShark.jar -open %s"%apkfile)
+        os.system("java -jar ../plugins/classyshark/ClassyShark.jar -open %s"%apkfile)
     else:
-        commands.getstatusoutput("java -jar ../plugins/classyshark/ClassyShark.jar")
+        os.system("java -jar ../plugins/classyshark/ClassyShark.jar")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
